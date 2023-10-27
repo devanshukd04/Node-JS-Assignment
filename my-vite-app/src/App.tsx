a14cc6cadf3ad05f6b838823748b9112d5f53eec
devanshukd04 <devanshukd27@gmail.com>
2023-10-28 00:46:47 +0530

    Additional changes

--- my-vite-app/src/App.tsx
+++ my-vite-app/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, redirect } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import SignUp from "./pages/SignUp.tsx";
 import SignIn from "./pages/SignIn.tsx";
